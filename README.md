2662bb396e6d9e46b4102f7cd21e2f8574902951
Brandon Robinson <brobinson@rdminfinity.com>
2022-10-31 11:24:18 -0700

    Initial Commit
    Initial commit of rdmBuild service

--- README.md
+++ README.md
@@ -1,2 +1,115 @@
-# rdmBuild
-RESTful API for Multivalue databases
+# README
+
+rdmBuild v2.0 service connector allows for calls into a Multivalue database to return formatted data to the calling end point. This version supports D3, jBase, Universe, QM, and ScarletDME. This is not a complete implentation of the RESTful API Spec. Requests are allowed via both POST and GET to the end point.
+
+The current build supports Linux, AIX, and Windows
+
+### Install Steps
+
+Copy the project into your desired directory. 
+
+> npm -i  
+
+This will install the needed packages and code to support rdmBuild.
+
+When complete run the following to lauch the service. Use tools like "forever" or "qckwinsvc" to run the program in the backgroud to respond to calls.
+
+> node riWweb2.js
+
+### Configure the service entry point for your system
+
+Create the service to control the MV database specifics for your enviroment. Note that you will need to match up your jBase enviroment paths to the correct env variables in jBase setups.
+
+#### D3 Sample (other samples in the "configs" folder for jBase, QM, etc...)
+
+rdm.json:
+
+	{
+	"setup" : {
+		"dbType" : "d3",
+        "dbBinary" : "/usr/bin/d3",
+        "dbUser" : "RDMWEB",
+		"dbAccount" : "RDM",
+        "dbPassword" : "INFINITY",
+        "dbVM" : "pick0"
+		}
+	}
+
+### Multivalue code install
+
+Once logged into your MV database create a file for the helper routines (not required but extremely helpful). Recommended location is RDM.LIBS
+
+Example (depending on our implementation): CREATE-FILE RDM.LIBS 3 11 
+
+Copy the install script inside of the mvCode into your favorite terminal emulator. This will create the following subroutines:
+ - RDM.GET.DATA
+  - A routine to convert any HTTP encoded in your varstring into useable MV arrays
+ - RDM.SEND.DATA
+  - A routine to send back data with the proper `~~START~~` and `~~END~~` tags through rdmBuild
+ - FORMAT.ERROR
+  - A routine that accepts a MV array of error messages that will format them in JSON or XML based on the options passed
+  
+- Compile and catalog the above routines
+ 
+## Sample MultiValue code to parse incoming variables and return a response
+
+**NOTE: The program must be cataloged so that the connector can call the program*
+
+	****
+	*
+	* XML.TEST.CONNECTION
+	*
+	********************************
+	*IF YOU DO NOT WANT TO USE THE SENTENCE() YOU CAN SET THIS UP AS PROC
+	*  Example (Change FILE.BP to the program location below):
+	*    
+	*  ED MD XML.TEST.CONNECTION
+	*     1 = PQ
+	*     2 = HRUN FILE.BP XML.TEST.CONNECTION
+	*     3 = P
+	********************************
+	* COMNMENT THIS OUT IF YOU USE A PROC
+	VARS = SENTENCE()
+	*
+	*  IF YOU USE A PROC STYLE ENTRY FOR THIS THEN REMOVE THE "TCL" IN THIS HELPER
+	*  EX: CALL RDM.GET.DATA(VARS,"")
+	*
+	CALL RDM.GET.DATA(VARS,"TCL")
+	LOCATE "var" IN VARS<1> SETTING POS THEN
+		VAR = VARS<2,POS>
+	END ELSE
+		VAR = ""
+	END
+	XML = '<success>':VAR:'</success>'
+	*or
+	JSON = '{"success":"':VAR:'"}"
+	*
+	*PASS THE XML OR JSON HERE TO SEND IT BACK IF USING THE HELPER FUNCTION
+	CALL RDM.SEND.DATA(XML,"")
+	STOP
+
+## Calling XML.TEST.CONNECTION via the API
+
+Using a tool such as Postman (https://www.getpostman.com/) or a web browser hit the following URL:
+
+- Localhost will be replaced with your MV server address. 
+- Port 9191 can be configured in your server.json file in the configs directory
+- SERVICE_NAME will correspond to the above service file that was created for your environment
+
+GET version:
+
+http://localhost:9191/service_name/XML.TEST.CONNCTION/?var=MV%20is%20cool
+
+POST version:
+
+POST http://localhost:9191/service_name/XML.TEST.CONNECTION
+
+var=MV%20is%20cool
+
+## Response from the XML.TEST.CONNECTION Call (XML or JSON)
+
+	<success>MV is cool</success>
+
+or (depending on what you send)
+
+	{"success":"MV is cool"}
---
+++ riWweb.js
@@ -0,0 +1,224 @@
+/*
+RDM Infinity, LLC
+Written by: Brandon Robinson
+Date: 07/01/22
+Unpublished copywrite stuff goes here
+
+Purpose: API server for Multivalue connections and stuff.
+Revisions:
+*/
+/* Get the included stuff here so we can use it later */
+//the web server component for routing and the static file handler
+const express = require("express");
+var serveStatic = require('serve-static')
+const bodyParser = require('body-parser');
+const url = require('url');
+const path = require('path');
+//template engine
+var pug = require('pug');
+//http request handler for pulling in data
+const axios = require('axios');
+//child processor handling
+const { spawn } = require('child_process');
+//file pluging to get the config files
+const fs = require('fs');
+
+var util = require('util');
+
+/*Set up the Express server to do the heavy lifing for the HTTP stuff*/
+var app = express();
+app.set('x-powered-by', false);
+app.set('x-powered-by', "RDM Infinity rdmBuild Connect - v2.0");
+app.use(bodyParser.raw({ inflate: true, type: '*/*' }));
+//status monitor
+app.use(require('express-status-monitor')({title:"RDM Infinity - Connection Monitor"}));
+//get the server.config file
+var serverConfig = "";
+
+try{
+  serverConfig = require("./configs/server.json");
+  console.log(serverConfig);
+}
+catch(e)
+{
+  console.log("!!!!!Server config file missing or mis-formatted!!!!!");
+  console.error("!!!!!Server not started!!!!!");
+  console.log(e);
+  process.exit(1);
+  return false;
+}
+
+/*LEAVE THESE 2 lines alone (or at least in this order) or you can't get the POST body*/
+//app.use(express.text());
+app.use("/asset",express.static("asset"));
+//used for backward compatibility
+app.use("/static",express.static("asset"));
+
+// //Setup the middleware to alway get the varString into the rawQuery property
+app.use(function(req,res,next){
+  if (req.method == "GET")
+  {
+    req.rawQuery = url.parse(req.url).query;
+  }
+  else
+  {
+    req.rawQuery = req.body.toString();
+  }
+  next();
+});
+//TODO  - Set port to configuration setting
+app.listen(serverConfig.server.serverPort,() =>  {
+  console.log("RDM Infinity rdmBuild v2.0 is Running on port "+serverConfig.server.serverPort);
+});
+
+//special situation to handle the new debug flags
+app.use("/:service/:programName/:format?/:debug?",(req,res,next) => {
+  console.log(req.body.toString()); 
+  //GET THE SERVICE SETUP FROM THE CONFIGS FOLDER ELSE PANIC
+  var service = req.params.service;
+  var programName = req.params.programName;
+  var serviceConfig = "";
+  var serviceLogging = false;
+  var serviceLogName = "";
+  var outputStarted = false;
+  FinalData = "";
+  try{
+    //serviceConfig = require("./configs/"+service+".json");
+    serviceConfig = fs.readFileSync("./configs/"+service+".json");
+    serviceConfig = JSON.parse(serviceConfig);
+    serviceLogging = serviceConfig.setup.serviceLogging;
+    serviceLogName = serviceConfig.setup.serviceLogName;
+    //console.log(serviceConfig.toString());
+  }catch(e){
+    console.log("Cannot find "+service+" config!!!")
+    res.status(500);
+    res.end("Service Configuration ERROR")
+  }
+  
+  if(serviceLogging)
+  {
+    console.log(`Service ${service} called and created.`);
+  }
+  var child = "";
+  switch(serviceConfig.setup.dbType)
+  {
+    case "d3":
+      var child = spawn(serviceConfig.setup.dbBinary, 
+              ["-n",
+              serviceConfig.setup.dbVM,
+              "-r",
+              "-d",
+              "\"\\f"+serviceConfig.setup.dbUser+"\\r"+serviceConfig.setup.dbPassword+"\\r"+req.params.programName.toUpperCase()+"\ "+req.rawQuery+"\\rexit\\r\"",
+              "-dcdon",
+              "-s"],
+              {
+                encoding : 'utf8',
+                shell: true,
+                env: process.env,
+                timeout: 10000
+            });
+    break;
+    case "jBase":
+      var child = spawn(serviceConfig.setup.dbBinary, 
+                  ["-",
+                  "-c",
+                  '"'+req.params.programName.toUpperCase()+"\ "+req.rawQuery+'"'],{
+                encoding : 'utf8',
+                shell: true,
+                env : serviceConfig.setup.env,
+                timeout: 10000
+            });  
+      break;
+      case "uv":
+          var child = spawn(serviceConfig.setup.dbBinary, 
+              ['"'+req.params.programName.toUpperCase()+"\ "+req.rawQuery+'"'],{
+                encoding : 'utf8',
+                shell: true,
+                env : serviceConfig.setup.env,
+                timeout: 10000
+            });  
+        break;
+        case "d3Win":
+          //for d3Win use the dbUser as the DB account name
+          var child = spawn(serviceConfig.setup.dbBinary, 
+            ["-n",
+              serviceConfig.setup.dbVM,
+              "-r",
+              "-d",
+              "\"\\f"+serviceConfig.setup.dbUser+"\\r"+serviceConfig.setup.dbPassword+"\\r"+req.params.programName.toUpperCase()+"\ "+req.rawQuery+"\\rexit\\r\"",
+              "-dcdon",
+              "-s"],
+            {
+              encoding : 'utf8',
+              shell: true,
+              env: process.env,
+              timeout: 10000
+          });
+          break;
+          case "qm":
+            //This could be used for ScarletDME also. Set the names of the configs to the correct thing
+            var child = spawn(serviceConfig.setup.dbBinary, 
+                ["-QUIET",
+                "-A"+serviceConfig.setup.dbVM,
+                req.params.programName.toUpperCase()+"\ "+req.rawQuery],{
+                  encoding : 'utf8',
+                  shell: true,
+                  env : serviceConfig.setup.env,
+                  timeout: 10000
+                });  
+            break;
+          default:
+            console.log("We don't suport this type. Try harder next time "+serviceConfig.setup.dbType);
+            res.status(500);
+            res.write({"error":"DB Type not supported. "+serviceConfig.setup.dbType+" is invalid for config."});
+            break;
+  }
+    if(req.params.format)
+    {
+      res.type(req.params.format.toString());
+    }
+    child.stdout.on('data', (data) => {
+      // data from the standard output is here as buffers
+      //console.log(process.env);
+      console.log("On data");
+      console.log(outputStarted);
+      //console.log(data.toString());
+      //FinalData += data.toString().replace(/[\u0000-\u001F\u007F-\u009F]/g, "");
+      data = data.toString().replace(/[\u0000-\u001F\u007F-\u009F]/g, "");
+      console.log(data);
+      //Steve is the magic man that came up with this MUCH smaller version of this. Removed from child.on.close so we can get the chunks of data as they process
+      if(!outputStarted)
+      {
+        //Except this. This was Brandons magic not Steve's
+        outputStarted = data.indexOf('~~START~~') > -1 ? true : false;
+      }
+      console.log(outputStarted);
+      
+      
+      var string = data.indexOf('~~START~~') > -1 ? data.split('~~START~~')[1] : data;
+      var output = string.split('~~END~~')[0];
+      if(outputStarted)
+      {
+        res.write(output);
+      }
+    });
+    
+    child.on('close', (code) => {
+      // console.log(`child process exited with code ${code}`);
+      console.log("Closing:");
+      console.log(code);
+      res.end();
+    });
+    //strange std out error happened. Panic...
+    child.stderr.on('data',(data)=> {
+       console.log("stderr hit");
+       console.log(data.toString());
+    });
+    // since these are streams, you can pipe them elsewhere
+    //child.stderr.pipe(dest);
+    child.on('error',(error)=>{
+      console.log(error);
+      res.status(500);
+      res.write({"error":"Default connection timeout limit. Check server config if needed."});
+   });
+});
